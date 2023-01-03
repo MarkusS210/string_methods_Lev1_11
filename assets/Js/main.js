d4290854a48095e8c919ef0ec26fc4f34e760ab6
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-03 14:43:54 +0100

    string_methods_Lev1_11

---
+++ assets/Js/main.js
@@ -0,0 +1,13 @@
+let x ="Hello"
+let y = "World"
+
+let z = x + y 
+console.log(z);
+document.write(z)
+
+let n = (x + " " + y);
+console.log(n);
+document.write (n)
+
+console.log (x + "World")
+document.write(x + y)
